4b36d3081d8e568b80638f2cbffc5899f585f1a8
Lars Martin S. Pedersen <larsmsp@gmail.com>
2017-12-15 10:54:30 +0100

    Optimize imports

--- src/main/kotlin/com/computas/kotlin/KotelettApplication.kt
+++ src/main/kotlin/com/computas/kotlin/KotelettApplication.kt
@@ -1,6 +1,5 @@
 package com.computas.kotlin
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
